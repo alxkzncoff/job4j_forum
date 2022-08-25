37a1ba7b678478def1bca5584e2c0827d80749f7
Aleksandr <alx.kzncoff@gmail.com>
2022-08-25 10:54:13 +0300

    add reg tests

---
+++ src/test/java/ru/job4j/forum/control/RegControlTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.forum.control;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.Main;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.persistence.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+class RegControlTest {
+
+    @MockBean
+    PasswordEncoder encoder;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void regShouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/reg"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("reg"));
+    }
+
+    @Test
+    public void regUser() throws Exception {
+        this.mockMvc.perform(post("/reg")
+                        .param("username", "User"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(argument.capture());
+        assertThat(argument.getValue().getUsername()).isEqualTo("User");
+    }
+}
